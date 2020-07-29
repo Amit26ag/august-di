94c8621bae30be1608d54787660fc79f06f43e8a
amit.sharma@hitachivantara.com <alliswell420idiots>
2020-07-30 02:02:25 +0530

    Multiple external properties files for Spring

--- src/main/java/com/august/augustdi/configuration/PropertyConfig.java
+++ src/main/java/com/august/augustdi/configuration/PropertyConfig.java
@@ -1,24 +1,38 @@
 package com.august.augustdi.configuration;
 
 import com.august.augustdi.domain.MusicProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
-@PropertySource("classpath:mymusic.properties")
+//@PropertySource("classpath:mymusic.properties")
+@PropertySources({
+        @PropertySource("classpath:mymusic.properties"),
+        @PropertySource("classpath:music2.properties")
+})
 public class PropertyConfig {
+
+    @Autowired
+    private Environment environment;
 
     @Value("${music.site}")
     String musicSite;
 
+    // Will pick form music2.properties as it appear later in the list
     @Value("${music.username}")
     String username;
 
     @Value("${music.password}")
     String password;
+
+    @Value("${Path}")
+    String sysUser;
 
     @Bean
     public MusicProvider musicProvider() {
@@ -27,7 +41,11 @@ public class PropertyConfig {
         musicProvider.setMusicSite(musicSite);
         musicProvider.setUsername(username);
         musicProvider.setPassword(password);
+
         System.out.println(musicProvider);
+
+        String pathEnvVar = environment.getProperty("Path");
+        System.out.println(String.join("\n", pathEnvVar == null ? new String[0] : pathEnvVar.split(";")));
         return musicProvider;
     }
 
